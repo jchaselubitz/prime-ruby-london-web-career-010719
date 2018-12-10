e362474bcdc26bc9286c03a0ab15d481087704d8
jchaselubitz <jchaselubitz@gmail.com>
2018-12-10 14:50:20 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
-# Add  code here!
+
+
+def prime?(number)
+  if number <= 1
+    answer = false
+  elsif primeTester(number) == number
+    answer = true
+  else
+    answer = false
+  end
+  puts answer
+  answer
+end
+
+def primeTester(number)
+  testNumber = 2
+    while number % testNumber != 0
+      puts testNumber
+      break if testNumber == number
+      testNumber += 1
+    end
+  puts testNumber
+  testNumber
+end
+
+ prime?(1763)
